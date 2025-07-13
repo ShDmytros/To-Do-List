d58987b29aa366ca06d6c3ee34630be49d295de2
Dmytro Shtuchka <160020732+ShDmytros@users.noreply.github.com>
2025-07-13 16:19:57 +0300

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const TASKS_LIST_SAVED = "tasks-list"
+
 const RenderList = () =>
 {
     const task_menu = document.querySelector('#task-menu');
@@ -7,7 +9,7 @@ const RenderList = () =>
     const list = document.querySelector('#list-of-tasks');
     list.classList.add("hide");
     
-    send_button.addEventListener("click", () => AddTask(message, list)); 
+    send_button.addEventListener("click", () => AddInputTask(list, message)); 
 
 
     const deleteAll = document.createElement('div');
@@ -15,57 +17,88 @@ const RenderList = () =>
     deleteAll.textContent = "ВИДАЛИТИ ВСЕ";
     deleteAll.addEventListener("click", () => DeleteAll(list)); 
     list.appendChild(deleteAll);
-}
 
-const RenderTasks = () =>
+    RenderTasks(list);
+};
+
+const AddInputTask = (list, message) =>
 {
-    const tasks = localStorage.key()
+    if (message.value === "" ) return;
+    AddTask(list, message.value);
+    message.value = "";
 }
 
-const AddTask = (message, list) =>
+const AddTask = (list, text) =>
 {
     const welcome = document.querySelector('#welcome');
-    if (message.value === "" ) return;
     list.classList.remove("hide");
     welcome.classList.add("hide");
 
     const group = document.createElement("div");
     group.classList.add("task");
     const task = document.createElement("li");
-    task.innerText = message.value;
-    console.log(message.value);
+    task.innerText = text;
     
     const delete_button = document.createElement("div");
     delete_button.innerText = "ВИДАЛИТИ";
     delete_button.classList.add("delete-button");
     delete_button.addEventListener("click", () => DeleteTask(list, group))
 
-    message.value = ""
-
     group.append(task, delete_button);
-    list.appendChild(group)
-}
+    list.appendChild(group);
 
+    UpdateTasks(list);
+};
 
 const DeleteTask = (list, el) =>
 {
-    localStorage.removeItem(el.querySelector("li").innerText);
     const welcome = document.querySelector('#welcome');
 
     list.removeChild(el)
 
+    UpdateTasks(list);
+
     if (list.querySelectorAll('.task').length === 0)
     {
         welcome.classList.remove("hide");
-        list.classList.add("hide");        
+        list.classList.add("hide");
     }
-
 }
 
 const DeleteAll = (list) =>
 {
     const tasks = list.querySelectorAll('.task');
     tasks.forEach(task => DeleteTask(list, task));
+};
+
+const UpdateTasks = (tasks_div) => 
+{
+    const tasks_li = tasks_div.querySelectorAll("li");
+    const tasks = [];
+
+    tasks_li.forEach(li =>
+    {
+        tasks.push(li.textContent.trim());
+        // console.log(li.textContent.trim());
+    });
+
+    localStorage.setItem(TASKS_LIST_SAVED, JSON.stringify(tasks));
+
+    if (JSON.parse(localStorage.getItem(TASKS_LIST_SAVED)).length === 0) {
+        localStorage.removeItem(TASKS_LIST_SAVED);
+    }
+};
+
+const GetTasks = () => JSON.parse(localStorage.getItem(TASKS_LIST_SAVED)) || [];
+
+const RenderTasks = (list) =>
+{
+    tasks = GetTasks();
+    
+    tasks.forEach(task =>
+    {
+        AddTask(list, task);
+    });
 }
 
 RenderList();
